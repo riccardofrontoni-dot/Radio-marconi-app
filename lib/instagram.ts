bdb32c9c6a2008b2760d106cbbdaa27d66af74e4
Alessio Lo Presti <alessiolp@mondoconvenienza.net>
2026-09-08 08:50:29 +0200

    Fix: Disabilitata la generazione statica sulle rotte Instagram

--- lib/instagram.ts
+++ lib/instagram.ts
@@ -3,19 +3,15 @@ export async function fetchInstagramData() {
   const accessToken = process.env.INSTAGRAM_ACCESS_TOKEN;
 
   if (!accountId || !accessToken) {
-    console.error("Variabili Instagram mancanti in process.env");
     return { followers: 0, media_count: 0 };
   }
 
   try {
     const url = `https://graph.facebook.com/v19.0/${accountId}?fields=followers_count&access_token=${accessToken}`;
-    
-    // Deselezioniamo la cache per avere i follower sempre aggiornati lato server
     const res = await fetch(url, { cache: "no-store" });
     const data = await res.json();
 
     if (data.error) {
-      console.error("Errore Graph API Instagram:", data.error.message);
       return { followers: 0, media_count: 0 };
     }
 
@@ -24,7 +20,6 @@ export async function fetchInstagramData() {
       media_count: 0,
     };
   } catch (error) {
-    console.error("Errore nella fetchInstagramData:", error);
     return { followers: 0, media_count: 0 };
   }
 }
